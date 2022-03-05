dd1e25adb3aa431f4f8ff4ff899480bdeb64b3d9
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2022-03-05 22:19:30 +0100

    feat: change method random word this new method use an array with the probability instead of the same database of the words

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -23,11 +23,12 @@ import { Mot } from '../shared/mot';
 export class HomePage implements OnInit {
   randomWord: Mot;
   try: string = '';
-
+  probabilityArray: number[] = [];
+  id: number;
   constructor(private _firebaseService: FirebaseService) {}
 
   ngOnInit(): void {
-    this.assignWordRandom();
+    this.getRandomWord();
   }
 
   objectsAreSame(x, y) {
@@ -41,85 +42,144 @@ export class HomePage implements OnInit {
     return objectsAreSame;
   }
 
-  sortArray(mots: Mot[]): Mot[] {
-    return mots.sort(function (a, b) {
-      return a.id - b.id || a.es.localeCompare(b.es);
+  // sortArray(mots: Mot[]): Mot[] {
+  //   return mots.sort(function (a, b) {
+  //     return a.id - b.id || a.es.localeCompare(b.es);
+  //   });
+  // }
+
+  getWordId() {
+    this._firebaseService.getMotId(this.id.toString()).subscribe((data) => {
+      this.randomWord = data;
     });
   }
 
-  getRandomWordArray(motArray: Mot[]): Mot {
-    let motSorted: Mot[] = this.sortArray(motArray);
-    let mot: Mot = this.getRandomWordSortedArray(motSorted);
-    return mot;
-  }
-
-  getRandomWordSortedArray(motArray: Mot[]): Mot {
-    let word: Mot | undefined;
-    let randomNumber = Math.random();
-    if (motArray.length > 1) {
-      word = this.foundWordPercentage(
-        motArray,
+  getRandomWord() {
+    this._firebaseService.probabilityJson.subscribe((data) => {
+      this.probabilityArray = JSON.parse(data[0][0]);
+      this.foundPercentage(
+        JSON.parse(data[0][0]),
         0,
-        motArray.length - 1,
-        randomNumber
+        JSON.parse(data[0][0]).length - 1,
+        1
       );
-    }
-
-    return word;
+      this.getWordId();
+    });
   }
+  // getRandomWordArray(motArray: Mot[]): Mot {
+  //   let motSorted: Mot[] = this.sortArray(motArray);
+  //   let mot: Mot = this.getRandomWordSortedArray(motSorted);
+  //   return mot;
+  // }
 
-  foundWordPercentage(
-    motArray: Mot[],
+  // getRandomWordSortedArray(motArray: Mot[]): Mot {
+  //   let word: Mot | undefined;
+  //   let randomNumber = Math.random();
+  //   if (motArray.length > 1) {
+  //     word = this.foundWordPercentage(
+  //       motArray,
+  //       0,
+  //       motArray.length - 1,
+  //       randomNumber
+  //     );
+  //   }
+
+  //   return word;
+  // }
+
+  foundPercentage(
+    probabilityArray: number[],
     left: number,
     right: number,
     randomNumber: number
-  ): Mot {
-    let word = motArray[left];
+  ) {
+    if (randomNumber == 1) {
+      randomNumber = Math.random();
+    }
     let pivot = parseInt(((right + left) / 2).toString());
     if (right - left == 1) {
-      this.randomWord = word;
-      return word;
-    }
-    if (right - left == 2) {
-      if (motArray[left].percentage < randomNumber) {
-        word = motArray[right];
+      this.id = left;
+    } else if (right - left == 1) {
+      if (probabilityArray[left] > randomNumber) {
+        this.id = left;
+      } else {
+        this.id = right;
       }
-      this.randomWord = word;
-      return word;
-    } else if (right - left > 2) {
-      if (motArray[pivot].percentage >= randomNumber) {
-        if (motArray[pivot - 1].percentage < randomNumber) {
-          word = motArray[pivot];
-          this.randomWord = word;
-          return word;
+    } else if (right - left > 1) {
+      if (probabilityArray[pivot] > randomNumber) {
+        if (probabilityArray[pivot - 1] < randomNumber) {
+          this.id = pivot;
         } else {
-          this.foundWordPercentage(motArray, left, pivot - 1, randomNumber);
+          this.foundPercentage(probabilityArray, left, pivot - 1, randomNumber);
         }
       } else {
-        this.foundWordPercentage(motArray, pivot + 1, right, randomNumber);
+        if (probabilityArray[pivot + 1] > randomNumber) {
+          this.id = pivot + 1;
+        } else {
+          this.foundPercentage(
+            probabilityArray,
+            pivot + 1,
+            right,
+            randomNumber
+          );
+        }
       }
     }
   }
+
+  // foundWordPercentage(
+  //   motArray: Mot[],
+  //   left: number,
+  //   right: number,
+  //   randomNumber: number
+  // ): Mot {
+  //   let word = motArray[left];
+  //   let pivot = parseInt(((right + left) / 2).toString());
+  //   if (right - left == 1) {
+  //     this.randomWord = word;
+  //     return word;
+  //   }
+  //   if (right - left == 2) {
+  //     if (motArray[left].percentage < randomNumber) {
+  //       word = motArray[right];
+  //     }
+  //     this.randomWord = word;
+  //     return word;
+  //   } else if (right - left > 2) {
+  //     if (motArray[pivot].percentage >= randomNumber) {
+  //       if (motArray[pivot - 1].percentage < randomNumber) {
+  //         word = motArray[pivot];
+  //         this.randomWord = word;
+  //         return word;
+  //       } else {
+  //         this.foundWordPercentage(motArray, left, pivot - 1, randomNumber);
+  //       }
+  //     } else {
+  //       this.foundWordPercentage(motArray, pivot + 1, right, randomNumber);
+  //     }
+  //   }
+  // }
 
   updateMot(word: Mot, tryWord: string) {
     let success: boolean = false;
     if (word.fr.trim() === tryWord.trim()) {
       success = true;
     }
+    console.log(word);
     this._firebaseService.updateMot(word, success, word.id.toString());
-    this.assignWordRandom();
+    this.getRandomWord();
     this.try = '';
   }
 
-  assignWordRandom() {
-    this._firebaseService.mots.pipe(first()).subscribe(
-      (data) => {
-        this.getRandomWordArray(data);
-      },
-      (error) => {
-        console.error(error);
-      },
-      () => {}
-    );
-  }
+  // assignWordRandom() {
+  //   this._firebaseService.mots.pipe(first()).subscribe(
+  //     (data) => {
+  //       this.getRandomWordArray(data);
+  //     },
+  //     (error) => {
+  //       console.error(error);
+  //     },
+  //     () => {}
+  //   );
+  // }
 }

3848031eec1c1f4b84f8789c883193eb3d0e128c
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2024-11-06 23:44:13 +0100

    :poop: feat: add header that must be improved in the future

--- src/app/home/models/home.interface.ts
+++ src/app/home/models/home.interface.ts
@@ -1,7 +1,9 @@
 import { DailyStats } from './daily-stats.interface';
 import { Goal } from './goal.interface';
+import { UserInfo } from './user-info.interface';
 
 export interface Home {
   weekStats: DailyStats[];
   goal: Goal;
+  userInfo: UserInfo;
 }

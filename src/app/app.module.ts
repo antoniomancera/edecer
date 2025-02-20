1ef226a8e2333c9a4b6baa5cf67b8a4bd4827d82
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-02-20 17:20:55 +0100

    :dizzy: add animation to card of quizz card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -18,6 +18,7 @@ import { TranslocoRootModule } from './transloco-root.module';
     AppRoutingModule,
     HttpClientModule,
     TranslocoRootModule,
+    BrowserAnimationsModule,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],

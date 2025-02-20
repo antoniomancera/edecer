2dc727f92677002d0aadd9ce296e5a0a8ccd0a54
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-02-20 17:20:20 +0100

    :bricks: move quizz-card component from shared to study component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-
 import { TranslocoModule } from '@jsverse/transloco';
 
-import { QuizCardComponent } from './components/quiz-card/quiz-card.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { WeekdayPipe } from './pipes/weekday.pipe';
 import { FirstChartUpperPipe } from './pipes/first-chart-upper.pipe';
 
 @NgModule({
-  declarations: [
-    QuizCardComponent,
-    LoadingComponent,
-    WeekdayPipe,
-    FirstChartUpperPipe,
-  ],
-  imports: [CommonModule, ReactiveFormsModule, TranslocoModule],
-  exports: [
-    QuizCardComponent,
-    LoadingComponent,
-    WeekdayPipe,
-    FirstChartUpperPipe,
-  ],
+  declarations: [LoadingComponent, WeekdayPipe, FirstChartUpperPipe],
+  imports: [CommonModule, TranslocoModule],
+  exports: [LoadingComponent, WeekdayPipe, FirstChartUpperPipe],
 })
 export class SharedModule {}

e86fac5167b594f2b61fa334c6dfc50a444b5dce
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-03-08 17:49:16 +0100

    :construction:test builder vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   BASE_URL: 'https://apprendre-backend.onrender.com',
-  SUPABASE_URL: process.env['SUPABASE_URL'] || '',
-  SUPABASE_KEY: process.env['SUPABASE_KEY'] || '',
+  SUPABASE_URL: process.env.SUPABASE_URL || '',
+  SUPABASE_KEY: process.env.SUPABASE_KEY || '',
 };

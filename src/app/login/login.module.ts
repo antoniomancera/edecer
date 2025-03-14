4db56a7f0366d45c90f6f1e03c328dac244bcf62
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-03-14 22:52:10 +0100

    :sparkles: add recover password logic

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -9,6 +9,12 @@ import { TranslocoModule } from '@jsverse/transloco';
 import { LoginPageRoutingModule } from './login-routing.module';
 import { LoginPage } from './login.page';
 import { SignupComponent } from './components/signup/signup.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { SharedModule } from '../shared/shared.module';
+import { LoginTitleComponent } from './components/login-title/login-title.component';
+import { SignupCompletedComponent } from './components/signup-completed/signup-completed.component';
+import { ResetPasswordCompletedComponent } from './components/reset-password-completed/reset-password-completed.component';
 
 @NgModule({
   imports: [
@@ -18,7 +24,16 @@ import { SignupComponent } from './components/signup/signup.component';
     IonicModule,
     LoginPageRoutingModule,
     TranslocoModule,
+    SharedModule,
   ],
-  declarations: [LoginPage, SignupComponent],
+  declarations: [
+    LoginPage,
+    SignupComponent,
+    SignupCompletedComponent,
+    ForgetPasswordComponent,
+    ResetPasswordComponent,
+    ResetPasswordCompletedComponent,
+    LoginTitleComponent,
+  ],
 })
 export class LoginPageModule {}

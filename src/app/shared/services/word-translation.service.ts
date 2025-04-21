6df74bb91ceefaf75ecf4dcb10d3a3093dff20f2
Antonio Mancera <antoniomanuelmancerahevilla@gmail.com>
2025-04-21 19:45:07 +0200

    :art: add UserInfo to explore Page

--- src/app/shared/services/word-translation.service.ts
+++ src/app/shared/services/word-translation.service.ts
@@ -14,47 +14,9 @@ import { environment } from 'src/environments/environment';
 })
 export class WordTranslationService {
   private readonly MOT_PALABRA = '/wordTranslation';
-  private readonly GET_RANDOM = '/getRandom';
-  private readonly ATTEMPS = '/attempts';
   private readonly PAGINATED = '/paginated';
 
   constructor(private http: HttpClient) {}
-
-  getRandomWordTranslation(deckId?: number): Observable<WordTranslation> {
-    return this.http
-      .get<WordTranslation>(
-        environment.BASE_URL + this.MOT_PALABRA + this.GET_RANDOM + '/' + deckId
-      )
-      .pipe(
-        map((wordTranslation) => {
-          return this.getWordTranslationWitParts(wordTranslation);
-        })
-      );
-  }
-
-  attemptsWordTranslation(
-    wordId: number,
-    phraseId: number,
-    success: boolean,
-    deckId: number
-  ): Observable<WordTranslation> {
-    const params = new HttpParams()
-      .set('phraseId', phraseId.toString())
-      .set('success', success)
-      .set('deckId', deckId);
-
-    return this.http
-      .put<WordTranslation>(
-        `${environment.BASE_URL}${this.MOT_PALABRA}${this.ATTEMPS}/${wordId}`,
-        null,
-        { params: params }
-      )
-      .pipe(
-        map((wordTranslation) => {
-          return this.getWordTranslationWitParts(wordTranslation);
-        })
-      );
-  }
 
   /**
    * Get All WordTranslation and their Phrases associated given a deck
@@ -90,20 +52,5 @@ export class WordTranslationService {
         '/' +
         pageSize
     );
-  }
-
-  private getWordTranslationWitParts(
-    wordTranslation: WordTranslation
-  ): WordTranslation {
-    const word = new WordTranslation(
-      wordTranslation.id,
-      wordTranslation.wordSenseFr,
-      wordTranslation.wordSenseSp,
-      wordTranslation.attempts,
-      wordTranslation.successes,
-      wordTranslation.phrase
-    );
-
-    return word.getWordTranslationWitParts();
   }
 }
